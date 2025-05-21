5772b4d1830c4025b55e2e57cc6c57e6a6bec57f
WindyDante <125888129+WindyDante@users.noreply.github.com>
2025-05-21 22:26:15 +0800

    feat: nacos 动态刷新注解局部式

--- cloud-demo/services/services-order/src/main/java/cn/ew/order/controller/OrderController.java
+++ cloud-demo/services/services-order/src/main/java/cn/ew/order/controller/OrderController.java
@@ -3,15 +3,30 @@ package cn.ew.order.controller;
 import cn.ew.order.bean.Order;
 import cn.ew.order.service.OrderService;
 import jakarta.annotation.Resource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope   // 激活远程nacos配置中心的自动刷新
 @RestController
 public class OrderController {
 
+
     @Resource
     private OrderService orderService;
+
+    @Value("${order.timeout}")
+    String orderTimeOut;
+
+    @Value("${order.auto-confirm}")
+    String orderAutoConfirm;
+
+    @GetMapping("/config")
+    public String config(){
+        return "order.timeout: " + orderTimeOut + ", order.auto-confirm: " + orderAutoConfirm;
+    }
 
     // 创建订单
     @GetMapping("/create")
