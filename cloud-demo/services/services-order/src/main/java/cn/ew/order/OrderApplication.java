b0ab871e78bd6e7c10755a098180c2bffcc1892f
WindyDante <125888129+WindyDante@users.noreply.github.com>
2025-05-22 14:53:31 +0800

    feat: 添加动态配置刷新监听器

--- cloud-demo/services/services-order/src/main/java/cn/ew/order/OrderApplication.java
+++ cloud-demo/services/services-order/src/main/java/cn/ew/order/OrderApplication.java
@@ -1,8 +1,16 @@
 package cn.ew.order;
 
+import com.alibaba.cloud.nacos.NacosConfigManager;
+import com.alibaba.nacos.api.config.ConfigService;
+import com.alibaba.nacos.api.config.listener.Listener;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 @SpringBootApplication
 @EnableDiscoveryClient // 开启服务发现
@@ -10,6 +18,35 @@ public class OrderApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OrderApplication.class, args);
+    }
+
+    // 项目启动就监听配置文件变化
+    // 发生变化后拿到变化值
+    // 发送邮件
+
+    @Bean
+    ApplicationRunner applicationRunner(NacosConfigManager nacosConfigManager){
+        // 函数式接口直接
+        return args ->{
+            ConfigService configService = nacosConfigManager.getConfigService();
+            // addListener(数据集Id,组Id,监听器);
+            configService.addListener("service-order.properties"
+                    , "DEFAULT_GROUP"
+                    , new Listener() {
+                        @Override
+                        public Executor getExecutor() {
+                            // 给出线程池大小
+                            return Executors.newFixedThreadPool(4);
+                        }
+
+                        @Override
+                        public void receiveConfigInfo(String configInfo) {
+                            // 接收配置信息
+                            System.out.println("变化的信息" + configInfo);
+                            System.out.println("邮件通知...");
+                        }
+                    });
+        };
     }
 
 }
--- cloud-demo/services/services-order/src/main/java/cn/ew/order/controller/OrderController.java
+++ cloud-demo/services/services-order/src/main/java/cn/ew/order/controller/OrderController.java
@@ -1,26 +1,31 @@
 package cn.ew.order.controller;
 
 import cn.ew.order.bean.Order;
-import cn.ew.order.properties.OrderProperties;
 import cn.ew.order.service.OrderService;
 import jakarta.annotation.Resource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-//@RefreshScope   // 激活远程nacos配置中心的自动刷新
 @RestController
+@RefreshScope   // 激活远程nacos配置中心的自动刷新
 public class OrderController {
+
 
     @Resource
     private OrderService orderService;
 
-    @Resource
-    private OrderProperties orderProperties;
+    @Value("${order.timeout}")
+    private String orderTimeOut;
+
+    @Value("${order.auto-confirm}")
+    private String orderAutoConfirm;
 
     @GetMapping("/config")
     public String config(){
-        return "order.timeout: " + orderProperties.getTimeout() + ", order.auto-confirm: " + orderProperties.getAutoConfirm();
+        return "order.timeout: " + orderTimeOut + ", order.auto-confirm: " + orderAutoConfirm;
     }
 
     // 创建订单
