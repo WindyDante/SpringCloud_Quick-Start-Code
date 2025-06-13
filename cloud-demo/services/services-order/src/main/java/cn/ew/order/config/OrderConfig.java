c836663f13e1ed3f4b1e265490a37d99cf02fdb5
WindyDante <125888129+WindyDante@users.noreply.github.com>
2025-06-13 14:11:11 +0800

    feat: 添加Feign重试器配置，支持自动重试机制

--- cloud-demo/services/services-order/src/main/java/cn/ew/order/config/OrderConfig.java
+++ cloud-demo/services/services-order/src/main/java/cn/ew/order/config/OrderConfig.java
@@ -1,15 +1,24 @@
 package cn.ew.order.config;
 
+
 import feign.Logger;
+import feign.Retryer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OrderConfig {
 
+    // 在容器中添加重试器,会自动识别
+    @Bean
+    Retryer retryer() {
+        // 返回默认的重试器
+        return new Retryer.Default();
+    }
+
     // 注意此处的依赖,FULL日志级别会打印所有信息,一般不开启
     @Bean
-    Logger.Level feignLoggerLevel(){
+    Logger.Level feignLoggerLevel() {
         // 设置Feign的日志级别为FULL
         return Logger.Level.FULL;
     }
