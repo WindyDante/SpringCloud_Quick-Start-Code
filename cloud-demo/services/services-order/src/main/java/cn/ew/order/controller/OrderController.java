be504bb22bb059cfceeb3d083b18d907fa342c2b
WindyDante <125888129+WindyDante@users.noreply.github.com>
2025-06-19 16:30:01 +0800

    feat: 热点规则限流测试，根据不同用户针对性给予限流

--- cloud-demo/services/services-order/src/main/java/cn/ew/order/controller/OrderController.java
+++ cloud-demo/services/services-order/src/main/java/cn/ew/order/controller/OrderController.java
@@ -42,8 +42,10 @@ public class OrderController {
     @GetMapping("/seckill")
     @SentinelResource(value = "seckill-order",fallback = "seckillFallback")
     public Order seckill(
-            @RequestParam Long userId,
-            @RequestParam Long productId
+            // 通常是根据参数来进行流控规则的,如果不携带某个参数,则不进行流控
+//            @RequestParam(defaultValue = "888") Long userId,
+            @RequestParam(required = false) Long userId,
+            @RequestParam(defaultValue = "666") Long productId
     ) {
         // 在Sentinel 中配置链路流控规则的高级选项，针对链路的入口资源设置，如果是seckill接口，就进行限制
         // 当然很多人会疑问为什么不直接对seckill接口进行限流，而是对createOrder方法进行限流？
